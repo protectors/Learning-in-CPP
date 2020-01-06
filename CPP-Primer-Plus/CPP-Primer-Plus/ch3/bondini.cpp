e360940ed288126eee5e79d292d94497f19a536a
protectors <1176136493@qq.com>
2020-01-06 08:19:56 +0800

    ch3

---
+++ CPP-Primer-Plus/CPP-Primer-Plus/ch3/bondini.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+	using namespace std;
+	cout<<"\007Operation \"HyperHype\" is now activated!\n";
+	cout<<"Enter your agent code:_________\b\b\b\b\b\b\b\b";
+	long code;
+	cin>>code;
+	cout<<"\007Your entered "<<code<<"...\n";
+	cout<<"\007Code verified! Proceed with Plan 23!\n";
+	return 0;
+}
