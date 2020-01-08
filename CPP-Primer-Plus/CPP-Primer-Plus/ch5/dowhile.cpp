dd33b9f3297dd89469eac75da11218a362767d77
protectors <1176136493@qq.com>
2020-01-08 15:20:09 +0800

    ch5.3 do while循环

---
+++ CPP-Primer-Plus/CPP-Primer-Plus/ch5/dowhile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+    using namespace std;
+    int n;
+    
+    cout<<"Enter numbers in the range 1-10 to find ";
+    cout<<"my favorite nubmer\n";
+    do{
+        cin>>n;
+    }while(n!=7);
+    cout<<"Yes, 7 is my favorite.\n";
+    return 0;
+}
