21774ebf9d472496f66b806021620b2fe80805ef
protectors <1176136493@qq.com>
2020-01-21 11:36:46 +0800

    ch10.2 对象和类

--- CPP-Primer-Plus/CPP-Primer-Plus/ch10/stock00.cpp
+++ CPP-Primer-Plus/CPP-Primer-Plus/ch10/stock00.cpp
@@ -49,7 +49,14 @@ void Stock::update(double price){
 }
 
 void Stock::show(){
-    std::cout<<"Company: "<<company
-             <<" Share Price: "<<shares<<'\n'
-             <<" Total Worth: "<<total_val<<'\n';
+    using std::cout;
+    using std::ios_base;
+    ios_base::fmtflags orig=cout.setf(ios_base::fixed, ios_base::floatfield);
+    std::streamsize prec=cout.precision(3);
+    cout<<"Company: "<<company
+        <<" Share Price: "<<shares<<'\n';
+    cout.precision(2);
+    cout<<" Total Worth: "<<total_val<<'\n';
+    cout.setf(orig, ios_base::floatfield);
+    cout.precision(prec);
 }
